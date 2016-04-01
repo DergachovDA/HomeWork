ea522fd1ff45a31ba141838bc90c011731265211
Dima Dergachov <dergachovda@gmail.com>
2016-04-01 15:07:56 +0300

    Add HappyTicket.java (8 exercise)

---
+++ src/ua/in/dergachovda/less04/HappyTicket.java
@@ -0,0 +1,25 @@
+package ua.in.dergachovda.less04;
+
+//8. Найти все счастливые билеты от 100000 до 999999
+
+public class HappyTicket {
+    public static void main(String[] args) {
+
+        for (int i = 100000; i < 1000000; i++){
+            int iLeft = i / 1000;
+            int iRight = i % 1000;
+            if (sumOfNumerals(iLeft) == sumOfNumerals(iRight))
+                System.out.println(i);
+        }
+    }
+
+    public static int sumOfNumerals(int n) {
+        int result = 0;
+        while (n > 0) {
+            result += n % 10;
+            n /= 10;
+        }
+        return result;
+    }
+
+}
