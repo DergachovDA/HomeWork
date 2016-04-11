2a45a3cce22038f2e57885ed8ace529567a57cb1
Dima D. <dergachovda@gmail.com>
2016-04-11 22:27:44 +0300

    add less07

---
+++ src/ua/in/dergachovda/less07/Spiral.java
@@ -0,0 +1,14 @@
+package ua.in.dergachovda.less07;
+
+public class Spiral {
+	
+	public static void main (String[] args) {
+		
+		int n = 10;
+		int[][] m = new int[n][n];
+		int x = m.length >> 1;
+		int y = m[x].length >>1;
+		
+	}
+	
+}
